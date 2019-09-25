64cd511830fe29d01ca3ac5f6573b3be504f6db7
kwasniew <kwasniewski.mateusz@gmail.com>
2019-09-25 15:41:47 +0200

    async await

--- src/app.js
+++ src/app.js
@@ -7,19 +7,19 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/booksapi';
 
 let books;
-MongoClient.connect(url, function(err, client) {
-    setTimeout(function() {
+MongoClient.connect(url, function (err, client) {
+    setTimeout(function () {
         books = client.db().collection("books");
     }, 10000);
 
 });
-let promise = MongoClient.connect(url).then(function(client) {
+let promise = MongoClient.connect(url).then(function (client) {
     return client.db().collection("books");
 });
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     // console.log("new requst at " + new Date());
     next();
 });
@@ -28,35 +28,37 @@ app.get("/", function (req, res) {
     res.send("Home");
 });
 
-app.post("/book", function(req, res) {
-    const {title, authors, isbn, description} = req.body;
-    promise.then(function(books) {
-        books.updateOne(
+app.post("/book", async function (req, res, next) {
+    try {
+        const {title, authors, isbn, description} = req.body;
+        const books = await promise;
+        await books.updateOne(
             {isbn: isbn},
-            { $set: {title, authors, isbn, description} },
+            {$set: {title, authors, isbn, description}},
             {upsert: true}
         );
-    });
+        res.json({title, authors, isbn, description});
+    } catch(e) {
+        next(e);
+    }
 
-
-    res.json({title, authors, isbn, description});
 });
 
-app.get("/book/:isbn", function (req, res) {
-    const isbn = req.params.isbn;
-    promise
-        .then(function (books) {
-            return books.findOne(
-                {isbn},
-                { projection: {_id: 0} }
-            );
-        })
-        .then(function (book) {
-            res.json(book);
-        });
+app.get("/book/:isbn", async function (req, res, next) {
+    try {
+        const books = await promise;
+        const isbn = req.params.isbn;
+        const book = await books.findOne(
+            {isbn},
+            {projection: {_id: 0}}
+        );
+        res.json(book);
+    } catch(e) {
+        next(e);
+    }
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     const error = new Error("not found");
     error.status = 404;
     next(error);
